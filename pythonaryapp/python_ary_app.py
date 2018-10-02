00303ca0d9578343db4a7e5cd6990b9fd1e54176
arycloud <abdul12391@gmail.com>
2018-10-02 17:00:46 +0500

    update routes

--- pythonaryapp/python_ary_app.py
+++ pythonaryapp/python_ary_app.py
@@ -3,14 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/python')
 def hello_world():
     return 'Hello World!'
 
 
-@app.route('/<name>/test')
-def user_name(name):
-    return 'Hello ' + name
 
 
 if __name__ == '__main__':
