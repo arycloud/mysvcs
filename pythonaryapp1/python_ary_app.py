a3bb87c4c00e069f5fc6d78381b40692b3af4c1e
arycloud <abdul12391@gmail.com>
2018-10-03 08:17:17 +0500

    add py svc and rm node svc

---
+++ pythonaryapp1/python_ary_app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/<name>')
+def hello_world(name):
+    return 'Your name is: {}'.format(name)
+
+
+
+
+if __name__ == '__main__':
+    app.run()
